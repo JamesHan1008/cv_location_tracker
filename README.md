007f132de8e1cdbfa6d1ed3dbc8323af82e1d084
JamesHan1008 <james@arceoanalytics.com>
2019-09-14 11:49:04 -0700

    Create combined video with map

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ python3 -m main
 
 
 # TO DO
+- Model detection in batch
+- Record a longer video
+
 - Let user select starting position on a map
-- Show video and predicted path on map simultaneously
 - Train model with additional stationary classes like "tree", "street light", etc.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import time
 
 import cv2
+import numpy as np
 import structlog
 
 # Path for importing Mask RCNN
@@ -38,29 +39,50 @@ model.load_weights(MODEL_PATH, by_name=True)
 
 # Load map
 map = cv2.imread("maps/map_hayward.png")
+map_height, map_width = map.shape[:2]
 x_prev = 370  # For SF: 600, for Hayward: 370
 y_prev = 198  # For SF: 815, for Hayward: 198
 theta_prev = 0
 
 # Read from a video
-video = cv2.VideoCapture("videos/intersection_stop_sign.MOV")
-fps = video.get(cv2.CAP_PROP_FPS)
-num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+video_in = cv2.VideoCapture("videos/intersection_stop_sign.MOV")
+fps = video_in.get(cv2.CAP_PROP_FPS)
+num_frames = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_height = int(video_in.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_width = int(video_in.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-frame_count = 0
-ds_memory = []
+# Write map to another video
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+video_out = cv2.VideoWriter(
+    "videos/map.avi",
+    fourcc,
+    fps,
+    (frame_width + map_width, frame_height + map_height),
+    isColor=True,
+)
 
 logger.info("Processing video", fps=fps, num_frames=num_frames, duration=num_frames/fps)
+frame_count = 0
+ds_memory = []
 t_start = time.time()
-while video.isOpened():
-    _, frame = video.read()  # X by Y by 3 (BGR)
+
+while video_in.isOpened():
+    _, frame = video_in.read()  # X by Y by 3 (BGR)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # TODO: model detection in batch
     detection_dict = model.detect([frame], verbose=1)[0]
+
     detection_set = DetectionSet(detection_dict)
     detection_set = filter_detections(detection_set)
 
     ds_memory.append(detection_set)
+
+    # combine the map with the video, and record it
+    combined_frame = np.zeros((frame_height + map_height, frame_width + map_width, 3), dtype="uint8")
+    combined_frame[:frame_height, :frame_width] = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    combined_frame[-map_height:, -map_width:] = map
+    video_out.write(combined_frame)
 
     # calculate movements every 6 frames, starting from the 7th frame
     if frame_count % 6 == 0 and frame_count != 0:
@@ -95,5 +117,6 @@ while video.isOpened():
         if frame_count >= 7:
             break
 
-video.release()
+video_in.release()
+video_out.release()
 cv2.destroyAllWindows()
